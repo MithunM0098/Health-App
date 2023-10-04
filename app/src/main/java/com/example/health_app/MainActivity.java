e6f3a6990a7331e2fe97bc57154d4ec922eefe9b
MMithun <mithun.m@checkpt.com>
2023-10-04 11:23:27 +0530

    Mithun commited

--- app/src/main/java/com/example/health_app/MainActivity.java
+++ app/src/main/java/com/example/health_app/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.health_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.badge.BadgeUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button logins;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logins=findViewById(R.id.logins);
+
+        logins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,User_deatils.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
